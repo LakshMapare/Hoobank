dd0065a7666993f5a98ae27ef3676b12e47bae16
LakshMapare <laxmikantmapare123@gmail.com>
2024-08-27 19:56:40 +0530

    hoobank

---
+++ src/Components/index.js
@@ -0,0 +1,28 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Testimonials from "./Testimonials";
+import States from "./States";
+
+
+export{
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  Hero,
+  Navbar,
+  Testimonials,
+  States
+  
+  
+}

6d75d4e8556cbc76b00cf7613b07ea7ed32bc280
LakshMapare <laxmikantmapare123@gmail.com>
2024-09-01 20:01:42 +0530

    added Hoobank

---
+++ src/constants/Constant.jsx
@@ -0,0 +1,54 @@
+import Bootstrap from "../assets/Bootstrap.png";
+import MaterialUi from "../assets/MaterialUi.png";
+import HTML from "../assets/HTML.png";
+import Github from "../assets/Github.png";
+import CSS from "../assets/CSS.png";
+import Sql from "../assets/Sql.png";
+import Javascript from "../assets/Javascript.png";
+import Git from "../assets/Git.png";
+import Vite from "../assets/Vite.png";
+import TailwindCSS from "../assets/TailwindCSS.png";
+import react from "../assets/React.png";
+import nextjs from "../assets/nextjs.png";
+
+import Hackthechain from "../assets/HackTheChainLogo.png";
+import GSSOC from "../assets/GSSoC.jpg";
+import IITB from "../assets/IITB.png";
+
+import IIITians from "../assets/IIITians_Network.png";
+import efficient from "../assets/efficient.jpg";
+import KDE from "../assets/kde.png";
+import Oppia from "../assets/oppia.png";
+
+import GPT4 from "../assets/gpt4.jpg";
+import Hoobank from "../assets/Hoobank.png";
+import cynthia from "../assets/cynthia.png";
+import quiz from "../assets/quiz.png";
+import WellBeing from "../assets/well-being.jpg";
+
+export {
+  Bootstrap,
+  MaterialUi,
+  HTML,
+  CSS,
+  Github,
+  Git,
+  Vite,
+  TailwindCSS,
+  react,
+  Sql,
+  Javascript,
+  nextjs,
+  Hackthechain,
+  GSSOC,
+  IITB,
+  IIITians,
+  GPT4,
+  Hoobank,
+  cynthia,
+  WellBeing,
+  quiz,
+  efficient,
+  KDE,
+  Oppia,
+};
